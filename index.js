72adc2ed6a019f46c2b1bfc074a868db4502c45b
kenechukwu ejeagwu <kinggleyejeagwu@gmail.com>
2024-01-20 15:24:44 +0100

    ejs practice

--- index.js
+++ index.js
@@ -1,62 +1,26 @@
 const http = require("http");
-const url = require("url");
 const fs = require("fs");
-const path = require("path");
+const ejs = require("ejs");
 
-// mimeType Object maps each extension to its appropriate content type
-const mimeType = {
-  ".ico": "image/x-icon",
-  ".html": "text/html",
-  ".js": "text/javascript",
-  ".json": "application/json",
-  ".css": "text/css",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".wav": "audio/wav",
-  ".mp3": "audio/mpeg",
-  ".svg": "image/svg+xml",
-  ".pdf": "application/pdf",
-  ".zip": "application/zip",
-  ".doc": "application/msword",
-  ".eot": "application/vnd.ms-fontobject",
-  ".ttf": "application/x-font-ttf",
-};
+// http
+//   .createServer((request, response) => {
+//     const file = fs.readFileSync(__dirname + "/views/home.ejs", "utf8");
+//     const output = ejs.render(file, { name: "kingsley" });
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.end(output);
+//   })
+//   .listen(3000);
 
 http
   .createServer((request, response) => {
-    // Parse URL
-    let parsedURL = url.parse(request.url, true);
-    console.log(request.url);
-    console.log(parsedURL);
-    // Remove Extra characters (for example /index.html/ becomes index.html)
-    let file_path = parsedURL.path.replace(/^\/+|\/+$/g, "");
-
-    // Serve index.html as default file
-    if (file_path == "") {
-      file_path = "index.html";
-    }
-
-    // Construct the file path
-    let file = __dirname + "/public/" + file_path;
-
-    // Read the file and return it on the callback
-    fs.readFile(file, function (err, content) {
-      if (err) {
-        response.writeHead(404);
-        response.end();
-      } else {
-        //specify the content type in the response
-        response.setHeader("X-Content-Type-Options", "nosniff");
-
-        // Extract the extension from the path of the file
-        const extension = path.parse(file).ext;
-
-        // Get the corresponding content-type
-        const mime = mimeType[extension];
-
-        response.writeHead(200, { "Content-type": mime });
-        response.end(content);
-      }
+    const file = fs.readFileSync(__dirname + "/views/hobbies.ejs", "utf8");
+    const output = ejs.render(file, {
+      data: {
+        name: "kingsley",
+        hobbies: ["football", "reading", "basketball"],
+      },
     });
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end(output);
   })
   .listen(3000);
